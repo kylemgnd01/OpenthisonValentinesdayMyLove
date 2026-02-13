3a7d4938baaf6975cb440b9bca4648fe40b2778c
kylemgnd01 <kyle.mgnda01@gmail.com>
2026-02-13 23:09:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -49,9 +49,9 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "Yay! I'm the luckiest person in the world!",
+        message: "Happy Valentines Day, my love! Thank you so much for being with me always. For being a large part of my life. Each year that we celebrate this together, I always reflect upon how much deeper my love has grown for you. Each day I spend with you, the more I fall in love with you. This year, may our dreams of being together come to fruition so that I can finally have more pictures of you pls pls Lord. T.T I LOVE YOU SOO SO SOO MUCHH ALWAYSS!! IM ALL YOURSS MWAAA MWAAAA",
+        emojis: "🤍​🥰​"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -69,7 +69,7 @@ const CONFIG = {
     animations: {
         floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
-        bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
+        bounceSpeed: "0.3s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
